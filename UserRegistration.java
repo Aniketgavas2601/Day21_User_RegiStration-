053c5a53ec201b5fe17029d9a90500d46b18371a
Experiment2601 <aniketgavas18@gmail.com>
2022-10-21 00:31:03 +0530

    UC5

--- UserRegistration.java
+++ UserRegistration.java
@@ -48,6 +48,19 @@ public class UserRegistration {
             System.out.println("Please Refer Above Format!!");
         }
     }
+    public void check_Password(String password) {
+
+        //Rule 1 - Minimum 8 Characters With At least Minimum 1 Uppercase
+        String regexPassword = "^[A-Z]{1}[a-zA-Z]{7,}$";
+        boolean result = password.matches(regexPassword);
+
+        if (result) {
+            System.out.println("Password Is Valid");
+        } else {
+            System.out.println("Password Is Invalid!!");
+            System.out.println("Please Enter Minimum 8 Characters With Atleast 1 Uppercase Letter");
+        }
+    }
 
 }
 
