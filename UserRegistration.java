c98aff27cdd2edd28d9386aa9797ecc661c38261
Experiment2601 <aniketgavas18@gmail.com>
2022-10-21 00:24:16 +0530

    UC2

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,4 @@
 package com.bridgelabz.UserRegistration;
-
-
-
 public class UserRegistration {
     public void minimumThreeCharacterNameWithFirstLetterIsCapital(String firstName) {
         String regexFirstName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
@@ -10,6 +7,16 @@ public class UserRegistration {
             System.out.println("It Is Valid First Name");
         } else {
             System.out.println("it Is Invalid First Name");
+        }
+    }
+
+    public void minimumThreeCharacterNameWithLasNameFirstIsCapital(String lastName) {
+        String regexLastName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
+        boolean result = lastName.matches(regexLastName);
+        if (result) {
+            System.out.println("It Is Valid Last Name");
+        } else {
+            System.out.println("It Is Invalid!! Please Enter Minimum Three Character Or Start Last Name With Capital");
         }
     }
 
